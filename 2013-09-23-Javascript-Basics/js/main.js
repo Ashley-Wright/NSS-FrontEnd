73a5ed4a9c857dfd4409c5d3b6f5e3cf494bf142
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-09-24 10:21:19 -0500

    Learning Branching

--- 2013-09-23-Javascript-Basics/js/main.js
+++ 2013-09-23-Javascript-Basics/js/main.js
@@ -3,6 +3,12 @@
 
 var first_name = prompt('What is your first name?');
 var last_name = prompt('What is your last name?');
+var gender = prompt("What is your gender?")
+var age = prompt("What is your age?")
+age = parseInt(age);
+var bday_month = prompt("What month were you born in?");
+var current_month = prompt("What month is it right now?");
+
 var full_name = first_name + " " + last_name;
 
 // debugger;
@@ -16,7 +22,50 @@ test2 = parseFloat(test2);
 var test3 = prompt("Score for Test 3?");
 test3 = parseFloat(test3);
 
-var average = (test1 + test2 + test3) / 3;
+var sum = 0;
+sum += test1;
+sum += test2;
+sum += test3;
+
+var average = (sum) / 3;
+
+if(average < 70)
+  console.log('failed');
+else if((average >= 70) && (average < 80))
+  console.log('you made a C');
+else if((average >=80) && (average < 90))
+  console.log('you made a B');
+else
+  console.log('you made an A');
+
 
 console.log("Your full name is : " + full_name);
 console.log("The average of your test scores is : " + average);
+
+var null_variable = null, underfined_variable;
+
+if((first_name == "Ashley") && (last_name == "Wright"))
+  console.log("hey, I know you!");
+
+if((gender == "female") && (age >= 21))
+  console.log("free drinks, ladies night!");
+else if((gender == 'male') && (age >= 21))
+  console.log("looks like you are buying!");
+else
+  console.log("not old enought to drink");
+
+var can_have_cake = (current_month == bday_month);
+var cake = can_have_cake ? "chocolate" : "vanilla";
+console.log("Marie Antoinette says that you are eating " + cake);
+
+switch(bday_month)
+{
+  case "January":
+    console.log("you are aquarious");
+    break;
+  case "Febuary":
+    console.log("you are pieces");
+    break;
+  default:
+    console.log("you are not of this world, god speed!");
+}
