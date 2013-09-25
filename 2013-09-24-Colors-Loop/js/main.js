82513562dbc595a766c1a3ffc816a1ed0e3feac7
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-09-24 21:38:02 -0500

    Learning about loops

---
+++ 2013-09-24-Colors-Loop/js/main.js
@@ -0,0 +1,23 @@
+
+var colors = [];
+
+var response = prompt('Enter a color');
+while(response)
+{
+  colors.push(response);
+  response = prompt('Enter a color or quit:');
+}
+
+for(var count = 0; count < colors.length; count++)
+{
+  console.log("You typed in color: " + colors[count]);
+}
+
+debugger;
+var sum = 0;
+for(var loop = colors.length - 1; loop >= 0; loop--)
+{
+  sum += colors[loop].length;
+}
+
+var avg = sum / colors.length;
