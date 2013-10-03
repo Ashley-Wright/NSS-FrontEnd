839c62853a87b9a9279efc7faae01869b7bb7fff
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-03 11:26:01 -0500

    Photoshop Lite with canvas

--- 2013-10-02-Photoshop-Lite/js/main.js
+++ 2013-10-02-Photoshop-Lite/js/main.js
@@ -1,8 +1,35 @@
+'use strict';
 
 $(document).ready(initialize);
 
 function initialize() {
-  $('#add_color').click(addColor)
+  $('#add_color').click(addColor);
+  $('#colors').on('click', '.box', colorPaletteClicked);
+  $('#add_box').click(addBox);
+  $('#boxes').on('mouseover', '.paintbox', canvasHover);
+}
+
+function canvasHover() {
+  var $canvas = $(this);
+  var brushColor = $('#brush').css('background-color');
+  $canvas.css('background-color', brushColor);
+}
+
+function addBox() {
+  var amount = $('#amount').val();
+  amount = parseInt(amount, 10);
+  for(var i = 0; i < amount; i++)
+  {
+    var $canvas = $('<div>');
+    $canvas.addClass('paintbox');
+    $('#boxes').append($canvas);
+  }
+}
+
+function colorPaletteClicked() {
+  var $box = $(this);
+  var color = $box.css('background-color');
+  $('#brush').css('background-color', color);
 }
 
 function addColor() {
