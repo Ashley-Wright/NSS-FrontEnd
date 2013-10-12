0b63d1ff5905153b28867255a42ef3670902ef26
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-12 18:19:44 -0500

    Stock Market, reorganized, no updated stock prices

--- 2013-10-10-Stock-Market/js/app/app.js
+++ 2013-10-10-Stock-Market/js/app/app.js
@@ -8,11 +8,8 @@ var Δquotes;
 
 // Local Schema
 var db = {};
-db.balance = [];
-db.balance.cash = 0;
-db.balance.bought = 0;
-db.balance.stocks = 0;
-// db.balance.total = 0;
+db.balance = {};
+db.balance.stock = 0;
 db.stocks = [];
 db.quotes = [];
 
@@ -29,53 +26,58 @@ function initialize(){
   Δquotes = Δdb.child('quotes');
 
   Δbalance.on('value', changeValue);
-  Δstocks.on('child_added', addStock);
+  Δstocks.on('child_added', stockAdded);
 }
 
-function changeValue(snapshot){
-  var balance = snapshot.val();
-  if(balance)
+function changeValue(balance){
+  balance = balance.val();
+
+  if(!balance)
   {
+    db.balance.cash = db.balance.stock = 0;
+  } else {
     db.balance = balance;
-    // db.balance.total = db.balance.cash - db.balance.bought + db.balance.stocks;
-    $('#balance').text(dollarAmount(db.balance.total));
   }
+  printBalance();
 }
 
-function addStock(snapshot){
-  var stock = snapshot.val();
-  var symbol = stock.symbol;
-
-  requestQuote(symbol, function(data, textStatus, jqXHR){
-    var quote = data.Data;
-
-    stock.currentPrice = quote.LastPrice;
-    createRow(stock);
-  });
+function stockAdded(stock){
+  stock = stock.val();
+  db.stocks.push(stock);
+  printAddStockRow(stock);
 }
 
-function createRow(stock) {
+// function addStock(snapshot){
+//   var stock = snapshot.val();
+//   var symbol = stock.symbol;
+
+//   requestQuote(symbol, function(data, textStatus, jqXHR){
+//     var quote = data.Data;
+
+//     stock.currentPrice = quote.LastPrice;
+//     createRow(stock);
+//   });
+// }
+
+
+function printAddStockRow(stock){
   var row = '<tr><td class="name"></td><td class="symbol"></td><td class="purchasedPrice"></td><td class="currentPrice"></td><td class="quantity"></td><td class="total"></td></tr>';
   var $row = $(row);
+  $row.addClass(stock.symbol).addClass('stock');
   console.log(stock);
 
-  var name = stock.name;
-  var symbol = stock.symbol;
-  var purchasedPrice = stock.purchasedPrice;
-  var currentPrice = stock.currentPrice;
-  var quantity = stock.quantity;
-  var total = stock.currentPrice * stock.quantity;
-
-  $row.children('.name').text(name);
-  $row.children('.symbol').text(symbol);
-  $row.children('.purchasedPrice').text(purchasedPrice);
-  $row.children('.currentPrice').text(currentPrice);
-  $row.children('.quantity').text(quantity);
-  $row.children('.total').text(total);
-
-  db.balance.stocks += total;
-  Δbalance.set(db.balance);
-  console.log(db.balance);
+  $row.children('.name').text(stock.name);
+  $row.children('.symbol').text(stock.symbol);
+  $row.children('.quantity').text(stock.quantity);
+  $row.children('.purchasedPrice').text(dollarAmount(stock.purchasedPrice));
+  if(!stock.currentPrice)
+  {
+    $row.children('.currentPrice').text(dollarAmount(stock.purchasedPrice));
+    $row.children('.total').text(dollarAmount(stock.purchasedPrice * stock.quantity));
+  } else {
+    $row.children('.currentPrice').text(dollarAmount(stock.currentPrice));
+    $row.children('.total').text(dollarAmount(stock.currentPrice * stock.quantity));
+  }
 
   $('#stocks').append($row);
 }
@@ -83,11 +85,8 @@ function createRow(stock) {
 function setFunds() {
   var balance = $('#setFunds').val();
   db.balance.cash = parseFloat(balance);
-  db.balance.total = db.balance.cash - db.balance.bought + db.balance.stocks;
-
-  $('#balance').text(dollarAmount(db.balance.total));
   Δbalance.set(db.balance);
-
+  $('#setFunds').val();
 }
 
 function purchase(){
@@ -99,19 +98,23 @@ function purchase(){
     var quote = data.Data;
     console.log(quote);
 
-    var stock = {};
-    stock.symbol = symbol;
-    stock.name = quote.Name;
-    stock.purchasedPrice = quote.LastPrice;
-    stock.quantity = quantity;
-    Δstocks.push(stock);
+    if(quote.LastPrice * quantity <= db.balance.cash){
 
-    db.balance.bought += stock.purchasedPrice * quantity;
-    db.balance.total = db.balance.cash - db.balance.bought + db.balance.stocks;
-    Δbalance.set(db.balance);
+      db.balance.cash -= quote.LastPrice * quantity;
+      db.balance.stock += quote.LastPrice * quantity;
+      Δbalance.set(db.balance);
+
+      var stock = {};
+      stock.symbol = symbol;
+      stock.name = quote.Name;
+      stock.purchasedPrice = quote.LastPrice;
+      stock.quantity = quantity;
+      Δstocks.push(stock);
+    }
 
     $('#symbol').val('');
     $('#quantity').val('');
+    $('#symbol').focus();
   });
 }
 
@@ -122,4 +125,10 @@ function requestQuote(symbol, fn){
 
 function dollarAmount(number){
   return '$' + number.toFixed(2);
+}
+
+function printBalance() {
+  $('#cash').val(dollarAmount(db.balance.cash));
+  $('#stock').val(dollarAmount(db.balance.stock));
+  $('#total').val(dollarAmount(db.balance.cash + db.balance.stock));
 }
