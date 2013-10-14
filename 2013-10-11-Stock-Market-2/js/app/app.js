3a7a874dbaaf7d6eb5460f9f2141b5073201b417
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-14 15:52:10 -0500

    Vacation maap

--- 2013-10-11-Stock-Market-2/js/app/app.js
+++ 2013-10-11-Stock-Market-2/js/app/app.js
@@ -4,10 +4,12 @@
 var Δdb;
 var Δbalance;
 var Δstocks;
+var Δphotos;
 
 // Local
 var db = {};
 db.stocks = [];
+db.photos = [];
 
 
 $(document).ready(initialize);
@@ -17,12 +19,27 @@ function initialize(){
   Δdb = new Firebase('https://stock-market-graph-amw.firebaseio.com/');
   Δbalance = Δdb.child('balance');
   Δstocks = Δdb.child('stocks');
+  Δphotos = Δdb.child('photos');
 
   $('#setButton').click(setFunds);
   $('#buyButton').click(buyStock);
+  // $('#graph').on('click', '.button', sellStock);
 
   Δbalance.on('value', balanceChanged);
+  // Δstocks.once('value', getStocks);
   Δstocks.on('child_added', stockAdded);
+}
+
+function getStocks(stocks){
+  var stocks = stocks.val();
+  var stock = {};
+  console.log(db.stocks);
+  for(stock in stocks)
+  {
+    console.log(stocks[stock]);
+    db.stocks.push(stocks[stock]);
+    console.log(db.stocks);
+  }
 }
 
 function balanceChanged(balance){
@@ -50,7 +67,7 @@ function buyStock(){
 
   requestQuote(symbol, function(data, textStatus, jqXHR){
     var quote = data.Data;
-    console.log(quote);
+    // console.log(quote);
 
     if(quote.LastPrice * quantity <= db.balance)
     {
@@ -64,6 +81,7 @@ function buyStock(){
       stock.price = quote.LastPrice;
 
       Δstocks.push(stock);
+      searchFlickr(stock.name);
     }
   });
 }
@@ -72,6 +90,44 @@ function requestQuote(symbol, fn){
   var data = {symbol: symbol};
   $.getJSON('http://dev.markitondemand.com/Api/Quote/jsonp?callback=?', data, fn);
 }
+
+function searchFlickr(name) {
+  // console.log(name);
+  var API_KEY = '1411583a1240294777887d50472970a0';
+  var PER_PAGE = 1;
+  var page = 1;
+
+  var query = name;
+  var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&text=' + query + '&per_page=' + PER_PAGE + '&page=' + page + '&format=json&jsoncallback=?';
+  $.getJSON(url, function(data){
+    // console.log(data);
+    var photo = data.photos.photo[0];
+    var url = 'url(http://farm'+ photo.farm +'.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
+    var photo = {};
+    photo.name = name;
+    photo.url = url;
+    // console.log(db.photos.url);
+    // console.log(db.photos.name);
+
+    Δphotos.push(photo);
+    db.photos.push(photo);
+    // console.log(db.photos);
+  });
+}
+
+// function results(data){
+  // createImage(data.photos.photo);
+  // var url = 'url(http://farm'+ photo.farm +'.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
+  // for(var i = 0; i < data.photos.photo.length; i++) {
+  //   createImage(data.photos.photo[i]);
+  // }
+// }
+
+// function createImage(photo){
+//   var url = 'url(http://farm'+ photo.farm +'.static.flickr.com/'+ photo.server +'/'+ photo.id +'_'+ photo.secret +'_m.jpg)';
+
+// }
+
 
 function stockAdded(stock){
   stock = stock.val();
@@ -99,7 +155,7 @@ function makeBarGraph(){
 
     var height = (db.stocks[stock].price / highestPrice) * base;
     var empty = base - height;
-    console.log(height);
+    // console.log(height);
 
     $color.css('height', height + 'px');
     $noColor.css('height', empty + 'px');
@@ -110,6 +166,17 @@ function makeBarGraph(){
     $button.addClass('small');
 
     var $img = $('<img>');
+    var stockName = db.stocks[stock].name;
+    // console.log(db.photos);s
+    // console.log(db.photos.photo);
+    // for(var i = 0; i < db.photos.length; i++)
+    // {
+    //   console.log(db.photos[i]);
+    //   if(db.photos[i].name === stockName){
+    //     var url = db.photos.url;
+    //   }
+    // }
+    // console.log(url);
     $img.attr('src', '#');
 
     $wrap.append($noColor);
@@ -119,3 +186,8 @@ function makeBarGraph(){
     $('#graph').append($wrap);
   }
 }
+
+// function sellStock(){
+//   var sell = $(this);
+
+// }
---
+++ 2013-10-14-Vacation-Map/public/js/app/app.js
@@ -0,0 +1,94 @@
+'use strict';
+
+// Firebase Schema
+var Δdb;
+var Δlocations;
+
+// Local Schema
+var db = {};
+db.locations = [];
+db.map = null;
+
+$(document).ready(initialize);
+
+function initialize(){
+  $(document).foundation();
+  Δdb = new Firebase('https://vacation-map-amw.firebaseio.com/');
+  Δlocations = Δdb.child('locations');
+  Δlocations.on('child_added', dbLocationAdded);
+  $('#set-zoom').click(clickSetZoom);
+  $('#add-location').click(clickAddLocation);
+  $('#go-location').click(clickGoLocation);
+  initMap(36, -86, 5);
+}
+
+function initMap(lat, lng, zoom){
+  var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
+  db.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+
+// ****Database**** //
+function dbLocationAdded(snapshot){
+  var location = snapshot.val();
+  db.locations.push(location);
+  htmlAddLocation(location);
+  htmlAddMarker(location);
+}
+
+
+// ****Event Handlers**** //
+function clickSetZoom(){
+  var zoom = getValue('#zoom', parseInt);
+  db.map.setZoom(zoom);
+}
+
+function clickAddLocation(){
+  var name = getValue('#location');
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({address: name}, function(results, status){
+    var location = {};
+    location.name = results[0].formatted_address;
+    location.coordinates = results[0].geometry.location;
+    Δlocations.push(location);
+  });
+}
+
+function clickGoLocation(){
+  var name = $('location-select').val();
+  var location = _.find(db.locations, function(l){return l.name === name;});
+  var latLng = new google.maps.LatLng(location.coordinates.lb, location.coordinates.mb);
+  db.map.setCenter(latLng);
+}
+
+
+// ****Change HTML**** //
+
+function htmlAddLocation(location){
+  var $option = $('<option>');
+  $option.val(location.name);
+  $option.text(location.name);
+  $('#location-select').append($option);
+}
+
+function htmlAddMarker(location){
+  var latLng = new google.maps.LatLng(location.coordinates.lb, location.coordinates.mb);
+  var marker = new google.maps.Marker({map: db.map, position: latLng, title: location.name});
+}
+
+
+function getValue(selector, fn){
+  var value = $(selector).val();
+  $(selector).val('');
+
+  if(fn){
+    value = fn(value);
+  }
+
+  return value;
+}
+
+function parseUpperCase(string){
+  return string.toUpperCase();
+}
