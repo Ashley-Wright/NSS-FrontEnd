cba8bf4707c5d680935c9fb8c9e262bb2da08961
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-30 18:48:43 -0500

    Express address book

---
+++ 2013-10-29-ExpressAddressBook/routes/people.js
@@ -0,0 +1,37 @@
+
+var database = require('../modules/database');
+
+/*
+*    GET /people
+*/
+
+exports.index = function(req, res){
+  var people = database.read(__dirname + '/../db/people.json');
+  res.render('people/index', { title: 'People: Address Book', people: people });
+};
+
+/*
+*    GET /people/new
+*/
+
+exports.new = function(req, res){
+  res.render('people/new', { title: 'New: Address Book'});
+};
+
+/*
+*    post /people
+*/
+
+exports.create = function(req, res){
+  var name = req.body.name;
+  var gender = req.body.gender;
+  var age = parseInt(req.body.age);
+  var color = req.body.color;
+
+  var people = database.read(__dirname + '/../db/people.json');
+  var person = {name: name, age: age, gender: gender, color:color};
+  people.push(person);
+  database.write(__dirname + '/../db/people.json', people);
+
+  res.redirect('/people');
+};
