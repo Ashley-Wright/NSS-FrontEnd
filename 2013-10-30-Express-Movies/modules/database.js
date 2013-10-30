068aacbf5400d1c1eecbdd711d79806ff9f8ef76
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-30 18:51:30 -0500

    Express Movies, more node practice

---
+++ 2013-10-30-Express-Movies/modules/database.js
@@ -0,0 +1,13 @@
+
+var fs = require('fs');
+
+exports.read = function(filename){
+  var data = fs.readFileSync(filename);
+  data = JSON.parse(data);
+  return data;
+};
+
+exports.write = function(filename, data){
+  data = JSON.stringify(data);
+  fs.writeFileSync(filename, data);
+};
