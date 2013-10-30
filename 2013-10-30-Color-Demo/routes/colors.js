b47058851865b9cdc7d57cb5c09ba5af259c0f69
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-30 18:50:32 -0500

    Color Demo, more node practice

---
+++ 2013-10-30-Color-Demo/routes/colors.js
@@ -0,0 +1,8 @@
+/*
+ * GET /colors
+ */
+
+exports.index = function(req, res){
+  var colors = ['blue', 'green', 'orange', 'olive', 'red'];
+  res.render('colors/index', {title: 'Colors', colors: colors});
+};
