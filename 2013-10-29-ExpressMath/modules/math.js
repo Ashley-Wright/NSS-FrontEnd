7c1aa53119380fff1c272aca5b21507210cdad3d
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-10-30 18:47:18 -0500

    Express Math

---
+++ 2013-10-29-ExpressMath/modules/math.js
@@ -0,0 +1,13 @@
+
+exports.square = function(x){
+  var sq = x * x;
+  return sq;
+};
+
+exports.area = function(length, width){
+  return length * width;
+};
+
+exports.vol = function(length, width, height){
+  return this.area(length, width) * height;
+}
